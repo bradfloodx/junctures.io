61e9e59aa7f91578a0f27d1f604da1100a775227
Bradley Flood <bradley.flood@gmail.com>
2017-12-07 13:40:32 +1100

    Adds basic header links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, {Component} from 'react';
 import Header from './Header';
-import './App.css';
+
+const links = [
+	{title: 'Sign In', path: '/sign-in'},
+	{title: 'Register', path: '/register'}
+];
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<Header/>
+				<Header links={links}/>
 			</div>
 		);
 	}
