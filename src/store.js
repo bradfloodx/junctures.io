fdea75ccb10a9caec6820e4f001b4043edf1161d
Bradley Flood <bradley.flood@gmail.com>
2017-12-07 20:53:35 +1100

    Add redux middlewares

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
+import {logger} from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
 
 import reducer from './reducers';
 
-export default createStore(reducer);
+export default createStore(reducer, applyMiddleware(logger, ReduxThunk));
