0ffb5a58d483221d1261517b994032e1eff508d0
Bradley Flood <bradley.flood@gmail.com>
2017-12-13 11:17:55 +1100

    Add juncture-list + test

---
+++ src/components/junctures/list.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {JuncturesList} from './list';
+
+const junctures = [
+	{
+		name: 'Holiday',
+		dateTime: 99,
+		id: 0
+	}
+];
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDOM.render(<JuncturesList junctures={junctures}/>, div);
+});
