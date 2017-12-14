8b0c20dab7a87b092828b176595ade02603214e1
Bradley Flood <bradley.flood@gmail.com>
2017-12-14 17:08:18 +1100

    Refactor routes

---
+++ src/routes.js
@@ -0,0 +1,26 @@
+export default {
+	home: {
+		title: 'Home',
+		path: '/'
+	},
+	register: {
+		title: 'Register',
+		path: '/register'
+	},
+	signIn: {
+		title: 'Sign In',
+		path: '/sign-in'
+	},
+	junctures: {
+		title: 'Junctures',
+		path: '/junctures'
+	},
+	juncturesCreate: {
+		title: 'Create',
+		path: '/junctures/new'
+	},
+	juncturesEdit: {
+		title: 'Edit',
+		path: '/junctures/1/edit'
+	}
+};
