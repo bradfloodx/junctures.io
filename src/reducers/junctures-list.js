57592108debc8f92d3bf9e5c42232a94ef910f97
Bradley Flood <bradley.flood@foxsports.com.au>
2017-12-15 12:06:38 +1100

    Refactor junctures and juncturelist

---
+++ src/reducers/junctures-list.js
@@ -0,0 +1,18 @@
+import actions from '../actions/types';
+
+const defaultState = [];
+
+const juncturesListReducer = (state = defaultState, action) => {
+	switch (action.type) {
+		case actions.JUNCTURES_LIST_FETCH:
+			return state;
+		case actions.JUNCTURES_LIST_FETCH_PENDING:
+			return state;
+		case actions.JUNCTURES_LIST_FETCH_FULFILLED:
+			return action.payload.data;
+		default:
+			return state;
+	}
+};
+
+export default juncturesListReducer;
