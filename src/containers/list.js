80b54b96f887790b5d1f141d3e20306c0b48171c
Bradley Flood <bradley.flood@gmail.com>
2017-12-15 17:48:58 +1100

    Link up juncture edit route

--- src/containers/list.js
+++ src/containers/list.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
+import {push} from 'react-router-redux';
 
 import JuncturesList from '../components/junctures/list';
+import {buildJuncturesEditRoute} from '../routes';
 
 const mapStateToProps = ({juncturesList}) => ({juncturesList});
 
-const JuncturesListContainer = ({juncturesList}) => (
+const mapDistpatchToProps = (dispatch) => ({
+	go: (id) => dispatch(push(buildJuncturesEditRoute(id)))
+});
+
+const JuncturesListContainer = ({juncturesList, go}) => (
 	<JuncturesList
 		junctures={juncturesList}
+		go={go}
 	/>
 );
 
@@ -20,7 +27,8 @@ JuncturesListContainer.propTypes = {
 			time: PropTypes.string,
 			id: PropTypes.string
 		})
-	)
+	),
+	go: PropTypes.func.isRequired
 };
 
-export default connect(mapStateToProps)(JuncturesListContainer);
+export default connect(mapStateToProps, mapDistpatchToProps)(JuncturesListContainer);
