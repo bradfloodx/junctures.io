7a59f937dbdd42c9ad98e9dd99931aa403500cc3
Bradley Flood <bradley.flood@foxsports.com.au>
2017-12-19 14:38:53 +1100

    Material UI can help

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 import React from 'react';
-import {render} from 'react-dom';
-import {ConnectedRouter} from 'react-router-redux';
-import {Provider} from 'react-redux';
+import { render } from 'react-dom';
+import { ConnectedRouter } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles';
 
-import {store, history} from './store';
+import { store, history } from './store';
 import App from './containers/app';
 
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
+const theme = createMuiTheme();
+
 // TODO
-// - Flow typings
-// - More Immutable
 // - Style (React UI Material Design?)
+// - Tests! (see redux docs + ...)
 // - Error messages and feedback
-// - Refactor actions/actions.js
 // - Home page
 // - Cordova App (build, icons + launch screens, store pages and screenshots, submit)
+// - Refactor actions/actions.js
+// - Flow typings
+// - More Immutable
 // -
 
 render((
 	<Provider store={store}>
 		<ConnectedRouter history={history}>
-			<App />
+			<MuiThemeProvider theme={theme}>
+				<App/>
+			</MuiThemeProvider>
 		</ConnectedRouter>
 	</Provider>
 ), document.getElementById('root'));
