74e173be9603c142ee89d6382052a50d92a31429
Bradley Flood <bradley.flood@gmail.com>
2017-12-20 19:09:35 +1100

    Extract distance function

--- src/components/junctures/juncture.js
+++ src/components/junctures/juncture.js
@@ -1,67 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import moment from 'moment';
-import 'moment-precise-range-plugin';
-
 import {
 	ListItem,
-	ListItemAvatar,
-	ListItemSecondaryAction,
-	ListItemText,
-	ListItemIcon
+	ListItemIcon,
+	ListItemText
 } from 'material-ui/List';
-import IconButton from 'material-ui/IconButton';
 import FlareIcon from 'material-ui-icons/Flare';
-import ModeEditIcon from 'material-ui-icons/ModeEdit';
 
-const displayDistance = (date, time) => {
-	const dateParts = date.split('-');
-	const timeParts = time.split(':');
-
-	const dateTimeParts = {
-		year: dateParts[0],
-		month: dateParts[1] - 1, // Month must be zero-indexed
-		day: dateParts[2],
-		hour: timeParts[0],
-		minute: timeParts[1]
-	};
-
-	const momentDateTime = moment(dateTimeParts);
-	const currentDateTime = moment();
-	const difference = moment.preciseDiff(momentDateTime, currentDateTime, true);
-
-	let distance = '';
-	distance += difference.years ? `${difference.years} years, ` : '';
-	distance += difference.months ? `${difference.months} months, ` : '';
-	distance += difference.days ? `${difference.days} days ` : '';
-	distance += difference.hours ? `${difference.hours} hours ` : '';
-	distance += difference.minutes ? `${difference.minutes} minutes ` : '';
-
-	return distance
-};
+import {displayDistance} from '../../utils';
 
 const JunctureWrapper = ({ name, date, time, id, go }) => (
 	<ListItem button={true} onClick={() => go(id)}>
-		<ListItemAvatar>
-			<ListItemIcon>
-				<FlareIcon/>
-			</ListItemIcon>
-		</ListItemAvatar>
+		<ListItemIcon>
+			<FlareIcon/>
+		</ListItemIcon>
 		<ListItemText
 			primary={name}
 			secondary={displayDistance(date, time)}
 		/>
-		<ListItemSecondaryAction
-			style={{display: 'none'}}
-		>
-			<IconButton
-				aria-label="Edit"
-				onClick={() => go(id)}
-			>
-				<ModeEditIcon/>
-			</IconButton>
-		</ListItemSecondaryAction>
 	</ListItem>
 );
 
