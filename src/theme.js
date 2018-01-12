d96255231adb38cca858d956866038ced81aa2f9
Bradley Flood <bradley.flood@gmail.com>
2018-01-12 17:28:32 +1100

    Create theme.js module

---
+++ src/theme.js
@@ -0,0 +1,27 @@
+export const COLORS = {
+	body: {
+		background: 'hsla(0, 0%, 19%, 1)',
+		color: 'hsla(211, 21%, 75%, 1)',
+		links: {
+			color: 'hsla(138,48%,50%,1)',
+		},
+		actions: {
+			primary: {
+				color: 'hsla(29, 63%, 52%, 1)',
+			},
+			secondary: {
+				color: 'hsla(280, 25%, 60%, 1)',
+			}
+		},
+	},
+	header: {
+		background: 'hsla(200, 4%, 28%, 1)',
+		links: {
+			color: 'hsla(138, 48%, 50%, 1)',
+			hoverBackground: 'hsla(200, 4%, 30%, 1)',
+		},
+	},
+	heading: {
+		color: 'hsla(38, 100%, 74%, 1)',
+	},
+};
