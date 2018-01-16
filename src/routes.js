86410a755aa69562752ca6aa24a0a205d30b77d9
Bradley Flood <bradley.flood@gmail.com>
2018-01-17 09:09:37 +1100

    Add Profile route

--- src/routes.js
+++ src/routes.js
@@ -7,17 +7,22 @@ export default {
 	register: {
 		title: 'Register',
 		path: '/register',
-		description: 'Register a new account on Junctures.io',
+		description: 'Register a new account on Junctures',
 	},
 	signIn: {
 		title: 'Sign In',
 		path: '/sign-in',
-		description: 'Sign in to your existing Junctures.io account',
+		description: 'Sign in to your existing Junctures account',
 	},
 	signOut: {
 		title: 'Sign Out',
 		path: '/sign-out',
-		description: 'Sign out of Junctures.io',
+		description: 'Sign out of Junctures',
+	},
+	profile: {
+		title: 'Profile',
+		path: '/profile',
+		description: 'Your Junctures profile',
 	},
 	juncturesCreate: {
 		title: 'Create',
