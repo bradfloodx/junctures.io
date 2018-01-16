ebc74f0a6fde3baf278b2d314ac5a5f4cc68f4e9
Bradley Flood <bradley.flood@gmail.com>
2018-01-16 17:13:59 +1100

    Add to Button proptypes and default props

--- src/components/common/button.js
+++ src/components/common/button.js
@@ -2,23 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-// import {COLORS} from '../../theme';
-
 const buttonHeight = 36;
 const buttonMinWidth = 64;
 
 const Button = styled.button`
 	display: inline-block;
+	padding: 0 16px;
 	height: ${buttonHeight}px;
 	min-width: ${buttonMinWidth}px;
 	border: none;
 	border-radius: 2px;
+	line-height: ${buttonHeight}px;
+	font-size: 14px;
 	color: #000;
 	background-color: hsla(0, 0%, 50%, 0.1);
-	padding: 0 16px;
-	font-size: 14px;
-	//font-family: sans-serif;
-	line-height: ${buttonHeight}px;
 	cursor: pointer;
 	transition: background-color ease-in-out 200ms;
 	
@@ -27,14 +24,20 @@ const Button = styled.button`
 	}
 `;
 
-const ButtonWrapper = ({children}) => (
-	<Button>
-		{children}
+const ButtonWrapper = ({text, type}) => (
+	<Button type={type}>
+		{text}
 	</Button>
 );
 
 ButtonWrapper.propTypes = {
-	children: PropTypes.node,
+	text: PropTypes.string.isRequired,
+	type: PropTypes.string,
+};
+
+ButtonWrapper.defaultProps = {
+	text: '',
+	type: '',
 };
 
 export default ButtonWrapper;
